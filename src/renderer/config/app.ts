b4a39c289109a761ce408a4b07f3586a905907cf
CrazyMarvin <git@poopjournal.rocks>
2023-11-13 23:06:46 +0100

    Update app.ts

--- src/renderer/config/app.ts
+++ src/renderer/config/app.ts
@@ -4,12 +4,14 @@ type DropdownConfigType = {
 }
 
 const countries: DropdownConfigType[] = [
-  { value: 'de', label: 'German' },
+  { value: 'de', label: 'Germany' },
 ]
 
 const languages: DropdownConfigType[] = [
   { value: 'en', label: 'English' },
   { value: 'si', label: 'Sinhala' },
+  { value: 'de', label: 'German' },
+  { value: 'no', label: 'Norwegian' },
 ]
 
 export {
