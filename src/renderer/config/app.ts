2fa86abb5235952dd4a9237d45446660f10cc302
CrazyMarvin <git@poopjournal.rocks>
2023-11-19 22:51:40 +0100

    Update API Endpoints

--- src/renderer/config/app.ts
+++ src/renderer/config/app.ts
@@ -5,9 +5,19 @@ type DropdownConfigType = {
 
 const countries: DropdownConfigType[] = [
   { value: 'de', label: 'Germany' },
+  { value: 'eu', label: 'European Union' },
+  { value: 'eu2', label: 'European Union 2' },
+  { value: 'us', label: 'United States' },
+  { value: 'ap', label: 'Asia/Pacific' },
+  { value: 'ca', label: 'Canada' },
+  { value: 'jp', label: 'Japan' },
+  { value: 'ae', label: 'United Arab Emirates' },
+  { value: 'fr', label: 'France' },
+  { value: 'au', label: 'Australia' },
 ]
 
 const languages: DropdownConfigType[] = [
+  { value: 'de', label: 'German' },
   { value: 'en', label: 'English' },
   { value: 'si', label: 'Sinhala' },
   { value: 'no', label: 'Norwegian' },
