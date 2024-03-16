03af2ca146d0cfcc467b3113f1fdb701dc570e11
fperera123 <mrlive119@ymail.com>
2024-03-16 07:40:52 +0530

    Add utility functions for opening new windows,sendLogout and managing redirects

--- src/renderer/lib/utils.ts
+++ src/renderer/lib/utils.ts
@@ -8,3 +8,25 @@ export function cn(...inputs: ClassValue[]) {
 export async function openFile(type: string, folder: string, group: string, filename: string) {
   await window.electron.ipcRenderer.invoke('ipc-open-file', type, folder, group, filename)
 }
+
+export function openNewWindow(path: string, width: number, height: number) {
+  window.electron.ipcRenderer.sendMessage('open-new-window', path, width, height)
+}
+
+export async function setRedirectTo (path: string) {
+  await localStorage.setItem('redirectTo', path)
+}
+
+export async function getRedirectTo () {
+  return localStorage.getItem('redirectTo')
+}
+
+export async function clearRedirectTo () {
+  await localStorage.removeItem('redirectTo')
+}
+
+export function sendLogout() {
+  window.electron.ipcRenderer.sendMessage('logout')
+}
+
+
